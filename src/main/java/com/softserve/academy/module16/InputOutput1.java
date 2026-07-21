b89afead3a310704f45b580ea3620395e1f69fee
Olha Shu <olya.shutileva@gmail.com>
2026-07-21 10:23:40 +0300

    upd in module 16

---
+++ src/main/java/com/softserve/academy/module16/InputOutput1.java
@@ -0,0 +1,21 @@
+package com.softserve.academy.module16;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class InputOutput1 {
+    public static void main(String[] args) throws IOException {
+        String fileName = "file.txt";
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String s = null;
+            int count = 0;
+            System.out.println("Read data from file: " + fileName);
+            while ((s = br.readLine()) != null) {
+                System.out.println("row " + ++count + " read:" + s);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
